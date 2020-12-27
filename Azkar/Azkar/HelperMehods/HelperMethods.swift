3bfe1bf001351ad739a67dc6b3abe6358b94dc7a
Kareem <k.fekry@bshub.com>
2020-12-27 20:48:12 +0200

    JSON FILE MODIFICATION

---
+++ Azkar/Azkar/HelperMehods/HelperMethods.swift
@@ -0,0 +1,25 @@
+//
+//  HelperMethods.swift
+//  Azkar
+//
+//  Created by Kareem on 12/27/20.
+//  Copyright © 2020 Kareem. All rights reserved.
+//
+
+import Foundation
+
+class HelperMethods {
+    class func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+}
---
+++ Azkar/Azkar/Models/AzkarModel.swift
@@ -0,0 +1,18 @@
+//
+//  AzkarModel.swift
+//  Azkar
+//
+//  Created by Kareem on 12/27/20.
+//  Copyright © 2020 Kareem. All rights reserved.
+//
+
+import Foundation
+
+class AzkarModel: Codable {
+    var category: String?
+    var count: String?
+    var description: String?
+    var reference: String?
+    var zekr: String?
+}
+
